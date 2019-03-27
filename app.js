149b721fe95e578d1e4e6f4042aa62020bc6f851
Chris Brinker <cmbrinker@gmail.com>
2019-03-27 15:01:31 +0100

    added delete and cleaned up

--- app.js
+++ app.js
@@ -66,5 +66,16 @@ app.put('/api/courses/:id', (req, res) => {
     res.send(course);
 });
 
+//DELETE COURSE DATA
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course was not found');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
